4ced3603ccd3a1f8df6abe8ec8f7936999e9218b
“tangwenjian” <tangwenjian@aspirecn.com>
2019-08-23 14:40:06 +0800

    x

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-// import '../css/style.less'
-
-const { join } = require('path')
-
-const fs = require('fs')
-// 读取目录下的index.html
-const path = join(__dirname + './../../pages')
-
-let files = fs.readdirSync(path) 
